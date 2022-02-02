6d43fade13b50b837e6a815da29ffe5e138636f2
Vivendum <vivendum@mail.ru>
2022-02-03 03:13:34 +0500

    add home page

--- shared/index.php
+++ shared/index.php
@@ -1,21 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="ru">
   <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Learning PHP</title>
+    <title lang="en">Hosting Vivendum</title>
   </head>
   <body>
-    <h1>Первые строки PHP кода на первом хостинге</h1>
-    <?php
-      include('01-console/001-run-file.php');
-      echo '<br><br>';
-      include('02-design/01-constant/001-PHP_EOL.php');
-      echo '<br><br>';
-      include('02-design/02-construct/001-print.php');
-      echo '<br><br>';
-      include('02-design/02-construct/002-echo.php');
-    ?>
+    <h1><a href="https://vivendum.github.io/present-myself"><abbr title="Vivendum">Мой</abbr></a> хостинг</h1>
+    <section>
+      <h2>Назначение сайта</h2>
+      <p>Изначально хостинг был предназначен только для обучения языку программирования <abbr title="Hypertext Preprocessor" lang="en">PHP</abbr> в практических условиях. Но в процессе что-то пошло не так и я запнувшись об создание собственного <span lang="en">codestyle</span> для <a href="https://packagist.org/packages/squizlabs/php_codesniffer" lang="en">codesniffer</a> на основе <a href="https://www.php-fig.org/psr/" lang="en"><abbr title="Hypertext Preprocessor Standard Recommendation">PSR</abbr></a> от <a href="https://www.php-fig.org/" lang="en"><abbr title="Hypertext Preprocessor Framework Interoperability Group">PHP-FIG</abbr></a> временно остановился и вернулся к истокам, а именно к изучению <span lang="en">frontend development</span>.</p>
+      <p>И в связи c изученем <abbr title="HyperText Markup Language" lang="en">HTML</abbr>, <abbr title="Cascading Style Sheets" lang="en">CSS</abbr> и <abbr title="JavaScript" lang="en">JS</abbr> у меня появилась потребность в экспериментах с учебным материалом. Поэтому предназначеннее хостинга стало шире.</p>
+    </section>
+    <section>
+      <h2>Навигация по сайту</h2>
+      <ul>
+        <li><em>Огромные</em> достижения в изучении <abbr title="Hypertext Preprocessor" lang="en">PHP</abbr> находятся <a href="/learning-php/learning-php.php">здесь</a>.</li>
+        <li>Файлохранилище <a href="files-storage/">здесь</a>. И так как у меня нет уверености нужна ли <a href="description-files.php">странница</a> с описанием назначения тех или иных файлов с сылками на них, я всё таки её создал на всякий случай.</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Задачи по улучшению сайта</h2>
+      <ul>
+        <li>Переименовать домен из learning-php в более подходящее по смыслу, например: <span lang="en">learning, learning-web, learning-vivendum</span> и т.д.</li>
+        <li>Представление обучения <abbr title="Hypertext Preprocessor" lang="en">PHP</abbr> ввиде сайта, следует кардинально пересмотреть и сделать его в виде конспекта с отображением полного описания эксперимента и его результата, а не так как сейчас - просто вывод результата.</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Важные замечания</h2>
+      <li>Все файлы страниц на сайте должны иметь расширение <code lang="en">.php</code></li>
+    </section>
   </body>
 </html>
