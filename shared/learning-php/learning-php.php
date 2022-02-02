4dd630c12d49a056327536769b163ef67f0e39a3
Vivendum <vivendum@mail.ru>
2022-02-03 03:22:30 +0500

    structure: ordering folder/file

---
+++ shared/learning-php/learning-php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP</title>
+  </head>
+  <body>
+    <h1>Первые строки PHP кода на первом хостинге</h1>
+    <?php
+      include('01-console/001-run-file.php');
+      echo '<br><br>';
+      include('02-design/01-constant/001-PHP_EOL.php');
+      echo '<br><br>';
+      include('02-design/02-construct/001-print.php');
+      echo '<br><br>';
+      include('02-design/02-construct/002-echo.php');
+    ?>
+  </body>
+</html>
